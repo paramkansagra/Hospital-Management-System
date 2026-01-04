917cade86a434a1d2b1386380541cd9df49ccb2f
paramkansagra <param.j.kansagra@gmail.com>
2026-01-04 19:06:42 +0530

    Implementing in memory security

---
+++ src/main/java/com/paramkansagra/Hospital/Management/System/config/PasswordEncodingConfig.java
@@ -0,0 +1,15 @@
+package com.paramkansagra.Hospital.Management.System.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncodingConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
